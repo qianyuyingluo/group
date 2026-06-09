3a9ca65320439459ee8c12480dbffc0e6c486689
qianyuyingluo <269201209+qianyuyingluo@users.noreply.github.com>
2026-06-10 00:36:51 +0800

    Set base URL for GitHub Actions deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? 'group';
+
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? `/${repositoryName}/` : '/',
   server: {
     port: 5173,
   },
